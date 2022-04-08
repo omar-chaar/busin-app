1ba4a1c6d57536e0784d47a12963c1201aa6702c
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-04-08 21:43:57 +0300

    Added login as default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo:'login',
+    pathMatch: 'full'
+  },    
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 @NgModule({
   imports: [
