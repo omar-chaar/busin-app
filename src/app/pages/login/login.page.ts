a40bbe3eb9b40a8ea58a95bed525c691fdbf83d3
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-04-14 21:18:58 +0300

    updated login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  redirectTo() {
+    this.router.navigateByUrl('/tabs/messages');
   }
 
 }
