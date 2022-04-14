cd61dd9f695020091b8a1a8e5f76e4322d2c9a02
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-04-14 21:19:17 +0300

    added message page

---
+++ src/app/pages/message/message.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.page.html',
+  styleUrls: ['./message.page.scss'],
+})
+export class MessagePage implements OnInit {
+
+  constructor(private _router: Router) { 
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
