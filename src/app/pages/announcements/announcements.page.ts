d1293f06dc8dd4438b97ffc6ec6da6188a35ce96
GabrielN11 <gabrielnunes11c@gmail.com>
2022-04-15 23:13:41 -0300

    login, announcements and contacts

--- src/app/pages/announcements/announcements.page.ts
+++ src/app/pages/announcements/announcements.page.ts
@@ -2,6 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { IonInfiniteScroll } from '@ionic/angular';
 import { IonAccordionGroup } from '@ionic/angular';
 
+interface IAnnouncement{
+  title: string,
+  text: string,
+  date: string,
+  read?: boolean,
+}
+
 @Component({
   selector: 'app-announcements',
   templateUrl: 'announcements.page.html',
@@ -11,7 +18,26 @@ export class AnnouncementsPage {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
 
-  items = [];
+  announcements: IAnnouncement[] = [
+    {
+      title: 'Update about clothing politics',
+      text: '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      date: '10/04/2022',
+      read: false,
+    },
+    {
+      title: 'New Safety Measures',
+      text: '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      date: '25/02/2022',
+      read: false,
+    },
+    {
+      title: 'Feast of Annunciation',
+      text: '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      date: '25/02/2022',
+      read: false,
+    },
+  ];
   numTimesLeft = 5;
 
   constructor() {
@@ -28,8 +54,12 @@ export class AnnouncementsPage {
   }
 
   addMoreItems() {
-    for (let i = 0; i < 20; i++)
-      this.items.push(i);
+    //for (let i = 0; i < 20; i++)
+      //this.items.push(i);
+  }
+
+  confirmRead(announcement: IAnnouncement): void{
+    announcement.read = true
   }
 
   logAccordionValue() {
--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonInfiniteScroll } from '@ionic/angular';
 import { IonAccordionGroup } from '@ionic/angular';
+
+interface IUser {
+  name: string,
+  position: string,
+}
+
+interface IDepartament {
+  name: string,
+  users: IUser[],
+}
+
 @Component({
   selector: 'app-contacts',
   templateUrl: 'contacts.page.html',
@@ -10,97 +21,135 @@ export class ContactsPage {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
 
-  users: {name: string, position: string}[]  = 
-  [{
-    "name": 'Aline Grover',
-    "position": 'Accountant',
-  },
-  {
-    "name": 'Nevada Anders',
-    "position": 'Manager',
-  },
-  {
-    "name": 'Nicholas Morissette',
-    "position": 'November 11, 2014',
-  },
-  {
-    "name": 'Rusty Umland',
-    "position": 'January 8, 2019',
-  },
-  {
-    "name": 'Amada Cerulli',
-    "position": 'July 22, 2009',
-  },
-  {
-    "name": 'Harriette Garcia',
-    "position": 'July 29, 2018',
-  },
-  {
-    "name": 'Shanelle Parodi',
-    "position": 'May 26, 2018',
-  },
-  {
-    "name": 'Roger Leite',
-    "position": 'August 6, 2015',
-  },
-  {
-    "name": 'Latina Faulcon',
-    "position": 'February 5, 2014',
-  },
-  {
-    "name": 'Jerrie Hoekstra',
-    "position": 'June 2, 2016',
-  },
-  {
-    "name": 'Domonique Byam',
-    "position": 'December 30, 2010',
-  },
-  {
-    "name": 'Monnie Bonar',
-    "position": 'December 20, 2018',
-  },
-  {
-    "name": 'Chu Kahle',
-    "position": 'November 17, 2017',
-  },
-  {
-    "name": 'Allan Passman',
-    "position": 'November 12, 2015',
-  },
-  {
-    "name": 'Conrad Caliendo',
-    "position": 'February 10, 2016',
-  },
-  {
-    "name": 'Elma Chenier',
-    "position": 'August 13, 2011',
-  },
-  {
-    "name": 'Wendi Hirano',
-    "position": 'July 27, 2018',
-  }
-];
+  departaments: IDepartament[] = [
+    {
+      name: 'Marketing',
+      users: [
+        {
+          name: 'Aline Groover',
+          position: 'Accountant'
+        },
+        {
+          "name": 'Nevada Anders',
+          "position": 'Manager',
+        }
+      ]
+    },
+    {
+      name: 'IT Departament',
+      users: [
+        {
+          name: 'Ahmed Hassan',
+          position: 'Senior Developer'
+        },
+        {
+          name: 'Omar El Khoury',
+          position: 'Junior Developer'
+        }
+      ]
+    },
+    {
+      name: 'Human Resources',
+      users: [
+        {
+          name: 'Harriette Garcia',
+          position: 'Recruiter'
+        }
+      ]
+    }
+  ]
+
+  users: IUser[] =
+    [{
+      "name": 'Aline Grover',
+      "position": 'Accountant',
+    },
+    {
+      "name": 'Nevada Anders',
+      "position": 'Manager',
+    },
+    {
+      "name": 'Nicholas Morissette',
+      "position": 'November 11, 2014',
+    },
+    {
+      "name": 'Rusty Umland',
+      "position": 'January 8, 2019',
+    },
+    {
+      "name": 'Amada Cerulli',
+      "position": 'July 22, 2009',
+    },
+    {
+      "name": 'Harriette Garcia',
+      "position": 'July 29, 2018',
+    },
+    {
+      "name": 'Shanelle Parodi',
+      "position": 'May 26, 2018',
+    },
+    {
+      "name": 'Roger Leite',
+      "position": 'August 6, 2015',
+    },
+    {
+      "name": 'Latina Faulcon',
+      "position": 'February 5, 2014',
+    },
+    {
+      "name": 'Jerrie Hoekstra',
+      "position": 'June 2, 2016',
+    },
+    {
+      "name": 'Domonique Byam',
+      "position": 'December 30, 2010',
+    },
+    {
+      "name": 'Monnie Bonar',
+      "position": 'December 20, 2018',
+    },
+    {
+      "name": 'Chu Kahle',
+      "position": 'November 17, 2017',
+    },
+    {
+      "name": 'Allan Passman',
+      "position": 'November 12, 2015',
+    },
+    {
+      "name": 'Conrad Caliendo',
+      "position": 'February 10, 2016',
+    },
+    {
+      "name": 'Elma Chenier',
+      "position": 'August 13, 2011',
+    },
+    {
+      "name": 'Wendi Hirano',
+      "position": 'July 27, 2018',
+    }
+    ];
   constructor() {
   }
 
-  loadContactsData(event) {    
+  loadContactsData(event) {
 
     //Loading event
     setTimeout(() => {
-      console.log('Done'); 
-      this.loadMoreContacts();    
+      console.log('Done');
+      this.loadMoreContacts();
       event.target.complete();
-    }, 2000);  
+    }, 2000);
   }
 
-  loadMoreContacts(){
-    for(let i = 0; i < 10; i++){
-     let name = "Extra contact " + i;
-     let position = "Extra position" + i;
-      this.users.push({"name":name,"position":position})
+  loadMoreContacts() {
+    for (let i = 0; i < 10; i++) {
+      let name = "Extra contact " + i;
+      let position = "Extra position" + i;
+      this.users.push({ "name": name, "position": position })
     }
   }
-  
+
   logAccordionValue() {
     console.log(this.accordionGroup.value);
   }
@@ -109,5 +158,5 @@ export class ContactsPage {
     this.accordionGroup.value = undefined;
   }
 
-    
+
 }
