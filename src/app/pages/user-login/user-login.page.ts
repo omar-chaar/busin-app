bc6dcdf8c7d1b0ba376623e46af609755f79cc07
GabrielN11 <gabrielnunes11c@gmail.com>
2022-04-16 23:09:38 -0300

    login page

--- src/app/pages/user-login/user-login.page.ts
+++ src/app/pages/user-login/user-login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginPage implements OnInit {
 
-  constructor() { }
+  email: string
+  password: string
+
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  async presentToast(message: string, duration: number, color: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration,
+      icon: 'information-circle',
+      position: 'top',
+      color
+    });
+    toast.present();
+  }
+
+  handleSubmit():void {
+    const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+
+    if(!this.email || !this.email.match(emailRegex)){
+      this.presentToast('Invalid e-mail format.', 4000, 'danger')
+      return
+    }
+    if(!this.password || this.password.length < 6){
+      this.presentToast('Your password is too short!', 4000, 'danger')
+      return
+    }
+
+    this.presentToast('Logged!', 4000, 'success')
+  }
 }
