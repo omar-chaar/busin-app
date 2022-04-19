19015e676a87726386d75f7f25f4153b23f9d2da
GabrielN11 <gabrielnunes11c@gmail.com>
2022-04-19 14:36:42 -0300

    create and join company pages

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { ValidationService } from 'src/app/services/validation/validation.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
-  constructor() { }
+  name: string;
+
+  constructor(private router: Router, private toastService: ToastService, 
+    private validationService: ValidationService) { }
 
   ngOnInit() {
+  }
+
+  handleSubmit():void{
+    if(this.validationService.validateLength('Name', this.name, 50)){
+      this.toastService.presentToast('Welcome to your new company!', 5000, 'success')
+      this.router.navigateByUrl('/tabs/messages')
+    }
+  }
+
+  redirectTo(url: string):void {
+    this.router.navigateByUrl(url);
   }
 
 }
