8505dcc037ade9051783d12f98185053a72964bc
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-05-26 18:09:07 +0300

    Fixed infinite scroll in announcements

--- src/app/pages/announcements/announcements.page.ts
+++ src/app/pages/announcements/announcements.page.ts
@@ -15,56 +15,128 @@ import { Router } from '@angular/router';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'reverse'
+  name: 'reverse',
 })
 export class ReversePipe implements PipeTransform {
-
   transform(value) {
-   if (!value) return;
+    if (!value) return;
 
-   return value.reverse();
+    return value.reverse();
   }
 }
 
 @Component({
   selector: 'app-announcements',
   templateUrl: 'announcements.page.html',
-  styleUrls: ['./announcements.page.scss']
+  styleUrls: ['./announcements.page.scss'],
 })
 export class AnnouncementsPage implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-  @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
+  @ViewChild(IonAccordionGroup, { static: true })
+  accordionGroup: IonAccordionGroup;
 
   announcements: Announcement[] = [];
-
+  loadedAnnouncements: Announcement[] = [];
   fullyLoaded = false;
-  page = 1
   modal: HTMLElement;
   user: User;
 
-  constructor(public modalController: ModalController, private announcementService: AnnouncementService,
-    private userService: UserService, private toastService: ToastService, private router: Router) {
-      this.user = this.userService.currentUser;
-      this.loadAnnouncements();
+  constructor(
+    public modalController: ModalController,
+    private announcementService: AnnouncementService,
+    private userService: UserService,
+    private toastService: ToastService,
+    private router: Router
+  ) {
+    this.user = this.userService.currentUser;
+    this.loadTenFirstAnnouncements();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  loadAnnouncements():void{
+  loadAnnouncements(): void {
     this.announcementService.getAnnouncements().subscribe(
-      (resp:any) => {
-        this.announcements = resp.data.map(announcement => {
+      (resp: any) => {
+        this.announcements = resp.data.map((announcement) => {
           const date = this.toDate(announcement.time);
-          announcement = new Announcement(announcement.announcement_id, announcement.announcement_title, announcement.announcement_body,
-             date, announcement.sender_id)
-          return announcement
-        })
+          announcement = new Announcement(
+            announcement.announcement_id,
+            announcement.announcement_title,
+            announcement.announcement_body,
+            date,
+            announcement.sender_id
+          );
+          return announcement;
+        });
         this.fullyLoaded = true;
-      },(err) => {
-          this.toastService.presentToast(err.error.error, 4500, 'danger');
-      })
+      },
+      (err) => {
+        this.toastService.presentToast(err.error.error, 4500, 'danger');
+      }
+    );
   }
+
+  loadTenFirstAnnouncements(): void {
+    this.announcementService.get10FirstAnnouncements().subscribe(
+      (resp: any) => {
+        this.announcements = resp.data.map((announcement) => {
+          const date = this.toDate(announcement.time);
+          announcement = new Announcement(
+            announcement.announcement_id,
+            announcement.announcement_title,
+            announcement.announcement_body,
+            date,
+            announcement.sender_id
+          );
+          return announcement;
+        });
+        this.announcements = this.announcements.reverse();
+        if (this.announcements.length < 10) {
+          this.fullyLoaded = true;
+        }
+      },
+
+      (err) => {
+        this.toastService.presentToast(err.error.error, 4500, 'danger');
+        this.fullyLoaded = true;
+      }
+    );
+  }
+
+  loadMore10Announcements(): void {
+    this.announcementService
+      .get10MoreAnnouncements(this.announcements[0].id)
+      .subscribe(
+        (resp: any) => {
+          this.loadedAnnouncements =  resp.data.map((announcement) => {
+            const date = this.toDate(announcement.time);
+            announcement = new Announcement(
+              announcement.announcement_id,
+              announcement.announcement_title,
+              announcement.announcement_body,
+              date,
+              announcement.sender_id
+            );
+            return announcement;
+          });
+            
+          if(this.loadedAnnouncements.length == 0){
+            return this.fullyLoaded = true;
+          }
+          this.announcements = [...this.loadedAnnouncements.reverse(),...this.announcements];
+
+          if (this.announcements[this.announcements.length - 1].id === this.loadedAnnouncements[this.loadedAnnouncements.length - 1].id) {
+             return this.fullyLoaded = true;
+          }
+               
+        },
+        (err) => {
+          this.toastService.presentToast(err.error.error, 4500, 'danger');
+          this.fullyLoaded = true;
+        }
+      );
+}
+
 
   async presentModal() {
     const modal = await this.modalController.create({
@@ -72,25 +144,45 @@ export class AnnouncementsPage implements OnInit {
       cssClass: 'my-custom-class',
       componentProps: {
         announcements: this.announcements,
-        user: this.user
-      }
+        user: this.user,
+      },
     });
     return await modal.present();
   }
 
   loadData(event): void {
+    console.log('Before loading');
+    console.log(this.announcements);
     if (!this.fullyLoaded) {
-      this.page += 1
-      setTimeout(() => {
-
+       console.log('Loading data...');  
+        setTimeout(() => {
         event.target.complete();
+        this.loadMore10Announcements();
+        console.log('Done');
+        console.log(this.announcements);
       }, 2000);
+      
+    } else {
+      event.target.disabled = true;
     }
+    
   }
 
   formatDate(date: Date): string {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
     let day = date.getDate().toString();
     let monthIndex = date.getMonth().toString();
     let year = date.getFullYear().toString();
@@ -98,35 +190,50 @@ export class AnnouncementsPage implements OnInit {
     let minutes = date.getMinutes().toString();
 
     if (day.length == 1) {
-      day = '0' + day
+      day = '0' + day;
     }
     if (hours.length == 1) {
-      hours = '0' + hours
+      hours = '0' + hours;
     }
-    if( minutes.length == 1) {
-      minutes = '0' + minutes
+    if (minutes.length == 1) {
+      minutes = '0' + minutes;
     }
 
-    return day + ' ' + monthNames[monthIndex] + ' ' + year + ' ' + hours + ':' + minutes;
+    return (
+      day +
+      ' ' +
+      monthNames[monthIndex] +
+      ' ' +
+      year +
+      ' ' +
+      hours +
+      ':' +
+      minutes
+    );
   }
-
 
   toDate(date: String): Date {
     const strArr = date.toString().split('T');
     const dateArr = strArr[0].toString().split('-');
     const timeArr = strArr[1].toString().split(':');
-    return new Date(+dateArr[0], +dateArr[1] - 1, +dateArr[2], +timeArr[0], +timeArr[1]);
+    return new Date(
+      +dateArr[0],
+      +dateArr[1] - 1,
+      +dateArr[2],
+      +timeArr[0],
+      +timeArr[1]
+    );
   }
 
   sortByDate(announcements: Announcement[]): void {
     const sortedArr = announcements.sort(function (a, b) {
       return b.date.getTime() - a.date.getTime();
     });
-    this.announcements = sortedArr
+    this.announcements = sortedArr;
   }
 
   openModal(): void {
-    if(this.user.admin)
-      this.presentModal()
+    if (this.user.admin) this.presentModal();
   }
 }
+
--- src/app/services/announcement/announcement.service.ts
+++ src/app/services/announcement/announcement.service.ts
@@ -20,6 +20,19 @@ export class AnnouncementService {
     return this.http.get<any[]>(url, { headers: headers });
   }
 
+  get10FirstAnnouncements(): Observable<any[]> {
+    const url = `${environment.apiUrl}/announcement/get-first-ten-announcements-for-user/${this.userService.currentUser.id}`;
+    const headers = {'Content-Type': 'application/json', 'authorization': 'Bearer ' + this.userService.currentUser.token};
+    return this.http.get<any[]>(url, { headers: headers });
+  }
+
+  get10MoreAnnouncements(announcementId: number):Observable<any[]> {
+    const url = `${environment.apiUrl}/announcement/get-next-ten-announcements-for-user/${this.userService.currentUser.id}/`;
+    const headers = {'Content-Type': 'application/json', 'authorization': 'Bearer ' + this.userService.currentUser.token};
+    const body = {announcementId: announcementId};    
+    return this.http.post<any[]>(url, body, { headers: headers });
+  }
+
   createAnnouncement(text: string, title: string):Observable<any>{
     const url = `${environment.apiUrl}/announcement/create`;
     const headers = {'Content-Type': 'application/json', 'authorization': 'Bearer ' + this.userService.currentUser.token};
@@ -27,4 +40,5 @@ export class AnnouncementService {
     return this.http.post(url, body, { headers: headers });
   }
 
+ 
 }
