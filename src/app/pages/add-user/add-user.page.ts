82c8831eb5b656897092d0ea8ab03370add0181a
GabrielN11 <gabrielnunes11c@gmail.com>
2022-05-28 17:42:19 -0300

    user reg code copy to clipboard

--- src/app/pages/add-user/add-user.page.ts
+++ src/app/pages/add-user/add-user.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController } from '@ionic/angular';
 import { DepartmentService } from 'src/app/services/department/department.service';
 import { ToastService } from 'src/app/services/toast/toast.service';
 import { UserService } from 'src/app/services/user/user.service';
@@ -27,20 +27,52 @@ export class AddUserPage implements OnInit {
 
   constructor(private modalController: ModalController, private departmentService: DepartmentService,
     private validationService: ValidationService, private toastService: ToastService,
-    private userService: UserService) {
-     
+    private userService: UserService, private actionSheetCtrl: ActionSheetController) {
+
   }
-  
+
   //Initialization and destroy functions
 
   ngOnInit() {
-    this.departments = this.departmentService.departments.filter(department => department.name !== 'Owner');   
+    this.departments = this.departmentService.departments.filter(department => department.name !== 'Owner');
   }
 
   dismiss() {
     this.modalController.dismiss({
       'dismissed': true
     });
+  }
+
+  async canDismiss(): Promise<void> {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: `Code generated: ${this.code}. Click to copy.`,
+      buttons: [
+        {
+          text: 'Copy',
+          role: 'destructive'
+        }
+      ]
+    });
+
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+
+    if (role === 'destructive') {
+
+      function copyToClipBoard(item:string) {
+        document.addEventListener('copy', (e: ClipboardEvent) => {
+          e.clipboardData.setData('text/plain', (item));
+          e.preventDefault();
+          document.removeEventListener('copy', null);
+        });
+        document.execCommand('copy');
+      }
+    
+      copyToClipBoard(this.code);
+      this.toastService.presentToast('Code copied to clipboard', 2000, 'success');
+      this.dismiss();
+    }
   }
 
   //Submit Functions
@@ -52,8 +84,8 @@ export class AddUserPage implements OnInit {
       this.validationService.validateLength('Surname', this.surname, 30, 2)) {
       this.userService.generateToken(this.name, this.surname, this.selectedDepartment.department_id, this.position, this.admin).subscribe(
         (response) => {
-          this.toastService.presentToast(response.response, 2000, 'success');
           this.code = response.data;
+          this.canDismiss();
         },
         (error) => {
           console.log(error)
